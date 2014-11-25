7383d8c453dfe73a54ad1d1a471c1ad42e831381
Nikita Dudnik <nikdudnik@gmail.com>
2014-11-25 15:44:55 +0300

    Setup packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var shell = require('gulp-shell')
 var downloadatomshell = require('gulp-download-atom-shell')
+var atom  = require('gulp-atom');
 
 gulp.task('downloadatomshell', function(cb){
 	downloadatomshell({
@@ -13,13 +14,25 @@ var demo_cl;
 
 switch (process.platform) {
 	case 'win32':
-		demo_cl = 'binaries\\atom.exe index.js';
+		demo_cl = 'binaries\\atom.exe src\\index.js';
 		break;
 	case 'darwin':
-		demo_cl = 'binaries/Atom.app/Contents/MacOS/Atom index.js';
+		demo_cl = 'binaries/Atom.app/Contents/MacOS/Atom src/index.js';
 		break;
 	default:
-		demo_cl = 'binaries/atom index.js';
+		demo_cl = 'binaries/atom src/index.js';
 };
 
 gulp.task('run', shell.task([ demo_cl ]));
+
+// Build project
+gulp.task('package', function() {
+  return atom({
+      srcPath: './src',
+      releasePath: './release',
+      cachePath: './cache',
+      version: 'v0.19.4',
+      rebuild: false,
+      platforms: ['darwin-x64', 'linux-ia32', 'linux-x64', 'win32-ia32']
+  });
+})
